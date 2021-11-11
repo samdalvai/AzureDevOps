b313678645c6eb533b0f78f90acc7c0c9ec1613b
Samuel Dalvai <samdalvai@unibz.it>
2021-11-11 20:33:03 +0100

    Update terraform configuration

--- main.tf
+++ main.tf
@@ -25,6 +25,11 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
+
+  # ensures that the resource is recreated before destruction
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Deploy multi-container App
@@ -38,10 +43,12 @@ module "web_app_container" {
   resource_group_name = azurerm_resource_group.rg.name
   plan = {
     name     = var.app_service_plan_name
-    sku_size = "S1"
+    sku_size = "B1"
   }
 
   name = var.web_app_container_name
+
+  start_time_limit = 600
 
   container_type = "compose"
 
